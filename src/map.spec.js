cfe8b6d0e85154904656761a38b03f15521a0fef
Suvarna221B <suvarnasivadas@gmail.com>
2021-08-31 18:29:25 +0530

    map.js and test added

--- src/map.spec.js
+++ src/map.spec.js
@@ -12,8 +12,17 @@ describe('Map', () => {
         expect(map([1,2,3],cube)).toEqual([1,8,27]);
     });
 
-    it('')
+    it('The result of cube func for input [] is []',()=>{
+        expect(map([],cube)).toEqual([]);
+    });
 
+    it('The result of identity func for input [1,2,3] is [1,2,3]',()=>{
+            expect(map([1,2,3],identity)).toEqual([1,2,3]);
+    });
+
+    it('The result of a given func for input array is the func applied to all the elements of the array',()=>{
+            expect(map([{x:10}],(obj)=>(obj.x + 1))).toEqual([11]git );
+    });
 
 })
 
